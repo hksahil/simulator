2b5454fa8e4661ec3753b203c8d3c3d8c8716741
Sahil-choudhary <32207288+hksahil@users.noreply.github.com>
2024-03-13 18:06:18 +0530

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -145,10 +145,21 @@ def sql_process(df):
     z_df1.rename(columns={'year_of_nsv':'yr_of_nsv', 'launch_year':'launch_yr','filtr':'filter','yearly_total':'new_nsv_year','next_mm_nsv':'new_nsv_wrap','nsv_yr_1': 'new_nsv_yr_1','nsv_yr_2': 'new_nsv_yr_2','nsv_yr_3': 'new_nsv_yr_3','nsv_yr_1_rollup': 'new_nsv_yr_1_rollup','nsv_yr_3_rollup': 'new_nsv_yr_3_rollup'}, inplace=True)
     return(z_df1)
 
-def plot_bar(df):
-    df['old_nsv_3yr_rollling']=df['old_nsv_3yr_rollling'].round(2)
-    df['new_nsv_3yr_rollling']=df['new_nsv_3yr_rollling'].round(2)
-    yr_nsv = df.groupby('Region')[['old_nsv_3yr_rollling', 'new_nsv_3yr_rollling']].sum().reset_index()
+def plot_bar(df,measure):
+    if measure == "1 year rolling nsv":
+        old_column = 'old_1 Year Rolling NSV'
+        new_column = 'new_1 Year Rolling NSV'
+        ylabel = '1 year nsv'
+        title = 'Old vs New NSV by Region (1 Year Rolling)'
+    elif measure == "3 year rolling nsv":
+        old_column = 'old_nsv_3yr_rollling'
+        new_column = 'new_nsv_3yr_rollling'
+        ylabel = '3 year nsv'
+        title = 'Old vs New NSV by Region (3 Year Rolling)'
+
+    df[old_column]=df[old_column].round(2)
+    df[new_column]=df[new_column].round(2)
+    yr_nsv = df.groupby('Region')[[old_column,new_column]].sum().reset_index()
     # Plotting
     fig, ax = plt.subplots(figsize=(10, 6))
     # Width of the bars
@@ -156,25 +167,36 @@ def plot_bar(df):
     # Index for the x-axis
     ind = range(len(yr_nsv))
     # Plotting old sales
-    old_sales = ax.bar(ind, yr_nsv['old_nsv_3yr_rollling']/4, bar_width, label='Old Sales')
-    new_sales = ax.bar([i + bar_width for i in ind], yr_nsv['new_nsv_3yr_rollling']/4, bar_width, label='New Sales')
+    old_sales = ax.bar(ind, yr_nsv[old_column]/4, bar_width, label='Old Sales')
+    new_sales = ax.bar([i + bar_width for i in ind], yr_nsv[new_column]/4, bar_width, label='New Sales')
     # Setting labels and title
     ax.set_xlabel('Region')
-    ax.set_ylabel('NSV 3 year rolling')
-    ax.set_title('Old vs New NSV by Region')
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
     ax.set_xticks([i + bar_width / 2 for i in ind])
     ax.set_xticklabels(yr_nsv['Region'])
     #ax.set(ylim=(10, 150))
     ax.legend()
     # Show plot
     st.pyplot()
-    yr_nsv['Difference'] = yr_nsv['new_nsv_3yr_rollling'] - yr_nsv['old_nsv_3yr_rollling']
+    yr_nsv['Difference'] = yr_nsv[new_column] - yr_nsv[old_column]
     st.dataframe(yr_nsv,height=175)
 
-def plot_bar2(df):
-    df['old_nsv_3yr_rollling']=df['old_nsv_3yr_rollling'].round(2)
-    df['new_nsv_3yr_rollling']=df['new_nsv_3yr_rollling'].round(2)
-    yr_nsv = df.groupby('yr_of_nsv')[['old_nsv_3yr_rollling', 'new_nsv_3yr_rollling']].sum().reset_index()
+def plot_bar2(df, measure):
+    if measure == "1 year rolling nsv":
+        old_column = 'old_1 Year Rolling NSV'
+        new_column = 'new_1 Year Rolling NSV'
+        ylabel = '1 year nsv'
+        title = 'Old vs New NSV by Region (1 Year Rolling)'
+    elif measure == "3 year rolling nsv":
+        old_column = 'old_nsv_3yr_rollling'
+        new_column = 'new_nsv_3yr_rollling'
+        ylabel = '3 year nsv'
+        title = 'Old vs New NSV by Region (3 Year Rolling)'
+
+    df[old_column]=df[old_column].round(2)
+    df[new_column]=df[new_column].round(2)
+    yr_nsv = df.groupby('yr_of_nsv')[[old_column,new_column]].sum().reset_index()
     # Plotting
     fig, ax = plt.subplots(figsize=(10, 6))
     # Width of the bars
@@ -182,20 +204,45 @@ def plot_bar2(df):
     # Index for the x-axis
     ind = range(len(yr_nsv))
     # Plotting old sales
-    old_sales = ax.bar(ind, yr_nsv['old_nsv_3yr_rollling']/4, bar_width, label='Old Sales')
-    new_sales = ax.bar([i + bar_width for i in ind], yr_nsv['new_nsv_3yr_rollling']/4, bar_width, label='New Sales')
+    old_sales = ax.bar(ind, yr_nsv[old_column]/4, bar_width, label='Old Sales')
+    new_sales = ax.bar([i + bar_width for i in ind], yr_nsv[new_column]/4, bar_width, label='New Sales')
     # Setting labels and title
-    ax.set_xlabel('Original Year of NSV')
-    ax.set_ylabel('NSV 3 year rolling')
-    ax.set_title('Old vs New NSV by Year of NSV')
+    ax.set_xlabel('yr_of_nsv')
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
     ax.set_xticks([i + bar_width / 2 for i in ind])
     ax.set_xticklabels(yr_nsv['yr_of_nsv'])
-    #ax.set(ylim=(0,60))
+    #ax.set(ylim=(10, 150))
     ax.legend()
     # Show plot
     st.pyplot()
-    yr_nsv['Difference'] = yr_nsv['new_nsv_3yr_rollling'] - yr_nsv['old_nsv_3yr_rollling']
+    yr_nsv['Difference'] = yr_nsv[new_column] - yr_nsv[old_column]
     st.dataframe(yr_nsv,height=175)
+
+def validate(df):
+    # duplicate project names 
+    errors = []
+    #Validate column names
+    expected_columns = ['Action', 'Proj_id', 'Project', 'launch_yr', 'launch_dt', 'Region', 'nsv_yr_1', 'nsv_yr_2', 'nsv_yr_3']
+    if list(df.columns) != expected_columns:
+        errors.append("Error: Column names should not change. \n\n \t\t Please keep the column name as same as the template")
+        return errors
+
+    # Validate no duplicate project names
+    if df['Project'].duplicated().any():
+       errors.append("Error: The Projects you entered already exist. \n\n \t\t Please change the new project name to run the simulator", )
+
+   # Validate no nulls in project names, project ids
+    columns_to_check = ['Project', 'Proj_id']
+    for column in columns_to_check:
+        if df[column].isnull().any():
+            errors.append(f"Error: There are null values in column '{column}'. \n\n  \t\t Please provide the values.")
+        
+    #Validate for string values in measure
+    if df['nsv_yr_1'].dtype == 'object' or df['nsv_yr_2'].dtype == 'object' or df['nsv_yr_3'].dtype == 'object' :
+        errors.append(f"Error: There are non-numerical values in NSV columns.\n\n  \t\t Please provide the numerical values.")
+    
+    return errors
 
 def main():
     #st.title('Kellogg POC Simulator')
@@ -217,10 +264,12 @@ def main():
     uploaded_file_df1 = st.sidebar.file_uploader(" ", type=["csv"])
 
     if uploaded_file_df1:
-            
-            df1 = pd.read_csv(uploaded_file_df1)
-            df1.sort_values(by='Action',ascending=True,inplace=True)
-
+        df1 = pd.read_csv(uploaded_file_df1)
+        df1.sort_values(by='Action',ascending=True,inplace=True)
+        if len(validate(df1))>0:
+            for i in validate(df1):
+                st.error(i)
+        else :
             # changing the data type and format of launch date
             df1['launch_dt'] = pd.to_datetime(df1['launch_dt'])
             df1['launch_dt'] = df1['launch_dt'].dt.strftime('%Y-%m-%d')
@@ -252,13 +301,16 @@ def main():
                 final = Process_data(sql_result,raw_copy)
                 final.sort_values(by='Proj_id',ascending=False,inplace=True)
                 final.drop(['flag'],axis=1,inplace=True)
-                #st.write('The Processed Data',final)
+
+                #Dropdown for the measure
+                measure_selected = st.selectbox("Select the measure: ",('1 year rolling nsv', '3 year rolling nsv'))
+                #st.write(measure_selected)
 
                 col1,col2=st.columns(2) 
                 with col1:
-                    plot_bar(final)
+                    plot_bar(final,measure_selected)
                 with col2:
-                    plot_bar2(final)
+                    plot_bar2(final,measure_selected)
                 st.write('The Processed Data',final)
     
 
